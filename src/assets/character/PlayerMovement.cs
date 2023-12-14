cfa58b21794cd0cbd520b2272dbf240778f9298a
georgesycabaccang <georgesycabaccang@gmail.com>
2023-12-15 00:09:47 +0800

    Work out how to generating tile programmatically.

--- src/assets/character/PlayerMovement.cs
+++ src/assets/character/PlayerMovement.cs
@@ -6,66 +6,143 @@ public partial class PlayerMovement : AnimatedSprite2D
 	String lastKeyPressed = "";
 	String stopPosition = "";
 
+	float movementAmountUpAndDown;
+	float movementAmountRightAndLeft;
+	float axisMovementAmount;
+
+	int numberOfKeysPressedAtTheSameTime = 0;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() { }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		float movementAmountUpAndDown = 0.01f;
-		float movementAmountRightAndLeft = 0.02f;
 
-		if (Input.IsKeyPressed(Key.Shift))
+		if (numberOfKeysPressedAtTheSameTime >= 3)
 		{
-			movementAmountUpAndDown = 0.03f;
-			movementAmountRightAndLeft = 0.04f;
-			this.SpeedScale = 1.5f;
-		}
-
-		if (!Input.IsAnythingPressed())
-		{
-			switch (lastKeyPressed)
-			{
-				case "w":
-					stopPosition = "idle_north_east";
-					break;
-				case "s":
-					stopPosition = "idle_south_west";
-					break;
-				case "a":
-					stopPosition = "idle_north_west";
-					break;
-				case "d":
-					stopPosition = "idle_south_east";
-					break;
-			}
-			this.Play(stopPosition);
+			movementAmountUpAndDown = 0;
+			movementAmountRightAndLeft = 0;
+			axisMovementAmount = 0;
 			this.Stop();
 		}
 
-		if (Input.IsKeyPressed(Key.W) && !Input.IsKeyPressed(Key.D) && !Input.IsKeyPressed(Key.A))
+		// modifier for sprite animation and movement speed. (AKA sprint)
+		if (!Input.IsKeyPressed(Key.Shift) && numberOfKeysPressedAtTheSameTime < 3)
 		{
-			this.Play("north_east");
-			this.Position += new Vector2(movementAmountRightAndLeft, -movementAmountUpAndDown);
+			movementAmountUpAndDown = 0.01f;
+			movementAmountRightAndLeft = 0.02f;
+			axisMovementAmount = 0.02f;
+		}
+		else if (Input.IsKeyPressed(Key.Shift) && numberOfKeysPressedAtTheSameTime < 3)
+		{
+			movementAmountUpAndDown = 0.03f;
+			movementAmountRightAndLeft = 0.04f;
+			axisMovementAmount = 0.05f;
+			this.SpeedScale = 1.5f;
+		}
+
+		numberOfKeysPressedAtTheSameTime = 0;
+
+		// handles stopping of movements _____________________
+		bool isMoving =
+			Input.IsKeyPressed(Key.W)
+			|| Input.IsKeyPressed(Key.D)
+			|| Input.IsKeyPressed(Key.A)
+			|| Input.IsKeyPressed(Key.S);
+
+		if (
+			!isMoving
+			|| (Input.IsKeyPressed(Key.D) && Input.IsKeyPressed(Key.A))
+			|| (Input.IsKeyPressed(Key.W) && Input.IsKeyPressed(Key.S))
+		)
+		{
+			this.Play();
+			this.SetFrameAndProgress(1, 1);
+		}
+		// ___________________________________________________
+
+		// *********************** direction per key pressed ************************
+		// x and y axis movement
+		if (
+			Input.IsKeyPressed(Key.W)
+			&& !Input.IsKeyPressed(Key.D)
+			&& !Input.IsKeyPressed(Key.A)
+			&& !Input.IsKeyPressed(Key.S)
+		)
+		{
+			numberOfKeysPressedAtTheSameTime++;
+			this.Play("north");
+			this.Position += new Vector2(0, -axisMovementAmount);
 			lastKeyPressed = "w";
 		}
-		if (Input.IsKeyPressed(Key.S) && !Input.IsKeyPressed(Key.D) && !Input.IsKeyPressed(Key.A))
+		if (
+			Input.IsKeyPressed(Key.S)
+			&& !Input.IsKeyPressed(Key.D)
+			&& !Input.IsKeyPressed(Key.A)
+			&& !Input.IsKeyPressed(Key.W)
+		)
 		{
-			this.Play("south_west");
-			this.Position += new Vector2(-movementAmountRightAndLeft, movementAmountUpAndDown);
+			numberOfKeysPressedAtTheSameTime++;
+			this.Play("south");
+			this.Position += new Vector2(0, axisMovementAmount);
 			lastKeyPressed = "s";
 		}
-		if (Input.IsKeyPressed(Key.A))
+		if (
+			Input.IsKeyPressed(Key.A)
+			&& !Input.IsKeyPressed(Key.S)
+			&& !Input.IsKeyPressed(Key.W)
+			&& !Input.IsKeyPressed(Key.D)
+		)
 		{
-			this.Play("north_west");
-			this.Position += new Vector2(-movementAmountRightAndLeft, -movementAmountUpAndDown);
+			numberOfKeysPressedAtTheSameTime++;
+			this.Play("west");
+			this.Position += new Vector2(-axisMovementAmount, 0);
 			lastKeyPressed = "a";
 		}
-		if (Input.IsKeyPressed(Key.D))
+		if (
+			Input.IsKeyPressed(Key.D)
+			&& !Input.IsKeyPressed(Key.S)
+			&& !Input.IsKeyPressed(Key.W)
+			&& !Input.IsKeyPressed(Key.A)
+		)
 		{
-			this.Play("south_east");
-			this.Position += new Vector2(movementAmountRightAndLeft, movementAmountUpAndDown);
+			numberOfKeysPressedAtTheSameTime++;
+			this.Play("east");
+			this.Position += new Vector2(axisMovementAmount, 0);
 			lastKeyPressed = "d";
 		}
+
+		// diagonal movement
+		if (Input.IsKeyPressed(Key.W) && Input.IsKeyPressed(Key.D))
+		{
+			numberOfKeysPressedAtTheSameTime += 2;
+			this.Play("north_east");
+			this.Position += new Vector2(movementAmountRightAndLeft, -movementAmountUpAndDown);
+			lastKeyPressed = "w+d";
+		}
+		if (Input.IsKeyPressed(Key.S) && Input.IsKeyPressed(Key.A))
+		{
+			numberOfKeysPressedAtTheSameTime += 2;
+			this.Play("south_west");
+			this.Position += new Vector2(-movementAmountRightAndLeft, movementAmountUpAndDown);
+			lastKeyPressed = "s+a";
+		}
+		if (Input.IsKeyPressed(Key.W) && Input.IsKeyPressed(Key.A))
+		{
+			numberOfKeysPressedAtTheSameTime += 2;
+			this.Play("north_west");
+			this.Position += new Vector2(-movementAmountRightAndLeft, -movementAmountUpAndDown);
+			lastKeyPressed = "w+a";
+		}
+		if (Input.IsKeyPressed(Key.S) && Input.IsKeyPressed(Key.D))
+		{
+			numberOfKeysPressedAtTheSameTime += 2;
+			this.Play("south_east");
+			this.Position += new Vector2(movementAmountRightAndLeft, movementAmountUpAndDown);
+			lastKeyPressed = "s+d";
+		}
+
+		// **********************************************************************
 	}
 }
--- src/maps/first-map/tile_map.cs
+++ src/maps/first-map/tile_map.cs
@@ -1,28 +1,74 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Godot;
 
 public partial class tile_map : TileMap
 {
+    enum ENTITY_TYPES
+    {
+        PLAYER = 1,
+        OBSTACLE = 2,
+        ITEM = 3
+    }
 
+    PackedScene obstacle = (PackedScene)
+        GD.Load("res://src/for_testing_purposes/obstacles_like_characters_in_a_cell.tscn");
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
+    Vector2 gridSize = new Vector2(16, 16);
+    List<int[]> gridArray = new List<int[]>();
 
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        Random randomInt = new Random();
 
-	}
+        int[] dummyArray = { };
+        Vector2I tileSize = TileSet.TileSize;
+        Vector2I halfTileSize = tileSize / 2;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		// if (Input.IsKeyPressed(Key.W))
-		// {
-		// 	GD.Print("test");
-		// 	theSprite.Play("north_east");
-		// }
-		// if (Input.IsKeyPressed(Key.S))
-		// {
-		// 	theSprite.Play("south_east");
-		// }
-	}
+        for (int i = 0; i < gridSize.X; i++)
+        {
+            gridArray.Add(dummyArray);
+
+            for (int j = 0; j < gridSize.Y; j++)
+            {
+                gridArray[i] = null;
+            }
+        }
+
+        Vector2[] obstaclePositions = new Vector2[5];
+
+        for (int i = 0; i < 5; i++)
+        {
+            Vector2 obstacleCoord = new Vector2(
+                randomInt.Next(0, (int)gridSize.X),
+                randomInt.Next(0, (int)gridSize.Y)
+            );
+            if (!obstaclePositions.Contains(obstacleCoord))
+            {
+                obstaclePositions[i] = obstacleCoord;
+            }
+        }
+
+        foreach (var item in gridArray)
+        {
+            GD.Print(item);
+        }
+
+        for (int i = 0; i < obstaclePositions.Length; i++)
+        {
+            CharacterBody2D newObstacle = (CharacterBody2D)obstacle.Instantiate();
+            newObstacle.Position = obstaclePositions[i] + halfTileSize;
+            GD.Print(newObstacle.Position);
+
+            // GD.Print(gridArray[(int)obstaclePositions[i].X][(int)obstaclePositions[i].Y]);
+            // gridArray[(int)obstaclePositions[i].X][(int)obstaclePositions[i].Y] = (int)
+            // 	ENTITY_TYPES.OBSTACLE;
+            AddChild(newObstacle);
+        }
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta) { }
 }
